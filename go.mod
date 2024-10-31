4bc9651d9b48c501a33ee664cfd1b05f3025c476
Dmitriy Arkhangelskiy <rerednaw69@gmail.com>
2024-10-31 18:12:47 +0100

    added camunda diagramm convertor

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/wanderer69/flow_processor
 
 go 1.22.5
 
-require github.com/stretchr/testify v1.9.0
+require (
+	github.com/stretchr/testify v1.9.0
+	github.com/tetratelabs/wazero v1.8.1
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
---
+++ pkg/camunda_7_convertor/helper.go
@@ -0,0 +1,25 @@
+package camunda7convertor
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/tetratelabs/wazero/api"
+)
+
+func logString(_ context.Context, m api.Module, offset, byteCount uint32) {
+	buf, ok := m.Memory().Read(offset, byteCount)
+	if !ok {
+		log.Panicf("Memory.Read(%d, %d) out of range", offset, byteCount)
+	}
+	fmt.Println(string(buf))
+}
+
+func getResult(_ context.Context, m api.Module, offset, byteCount uint32) {
+	buf, ok := m.Memory().Read(offset, byteCount)
+	if !ok {
+		log.Panicf("Memory.Read(%d, %d) out of range", offset, byteCount)
+	}
+	fmt.Println(string(buf))
+}
