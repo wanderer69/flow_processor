67947e86aa3e2fa3e4029f8175b31f716b8ed20a
Dmitriy Arkhangelskiy <rerednaw69@gmail.com>
2024-11-11 13:58:01 +0300

    added url to client

--- pkg/entity/context.go
+++ pkg/entity/context.go
@@ -4,3 +4,9 @@ type Context struct {
 	VariablesByName map[string]*Variable
 	MessagesByName  map[string]*Message
 }
+
+type ProcessorContext struct {
+	ProcessName string
+	ProcessID   string
+	CurrentTask string
+}
