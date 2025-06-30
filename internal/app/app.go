c968931e8cb51e010ebce55e633d42ac0507cc26
Dmitriy Arkhangelskiy <rerednaw69@gmail.com>
2025-06-30 19:55:22 +0300

    remove comments

--- internal/app/app.go
+++ internal/app/app.go
@@ -177,81 +177,8 @@ func (a *Application) Init(appCtx context.Context, appCancel context.CancelFunc,
 	return nil
 }
 
-/*
-	type helloServer struct {
-		pb.UnimplementedGreeterServer
-	}
-
-	func (*helloServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-		fmt.Printf("sayHello: %v\r\n", in.Name)
-		return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-	}
-*/
 func (a *Application) Start() {
 	logger := zap.L()
-
-	/*
-		go func() {
-				//App context setup
-				appCtx, appCancel := context.WithCancel(context.Background())
-				defer appCancel()
-
-				//Setup HTTP / Websocket server
-				router := http.NewServeMux()
-
-				wsl := wasmws.NewWebSocketListener(appCtx)
-				router.HandleFunc("/grpc-proxy", wsl.ServeHTTP)
-				router.Handle("/", http.FileServer(http.Dir("./spa")))
-				httpServer := &http.Server{Addr: ":8188", Handler: router}
-				//Run HTTP server
-				go func() {
-					defer appCancel()
-					log.Printf("ERROR: HTTP Listen and Server failed; Details: %s", httpServer.ListenAndServe())
-				}()
-
-				//gRPC setup
-				creds, err := credentials.NewServerTLSFromFile("cert.pem", "key.pem")
-				if err != nil {
-					log.Fatalf("Failed to contruct gRPC TSL credentials from {cert,key}.pem: %s", err)
-				}
-				grpcServer := grpc.NewServer(grpc.Creds(creds))
-				pb.RegisterGreeterServer(grpcServer, new(helloServer))
-				//Run gRPC server
-				go func() {
-					defer appCancel()
-
-					if err := grpcServer.Serve(wsl); err != nil {
-						log.Printf("ERROR: Failed to serve gRPC connections; Details: %s", err)
-					}
-				}()
-
-				//Handle signals
-				sigs := make(chan os.Signal, 1)
-				signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-				go func() {
-					log.Printf("INFO: Received shutdown signal: %s", <-sigs)
-					appCancel()
-				}()
-
-				//Shutdown
-				<-appCtx.Done()
-				shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), time.Second*2)
-				defer shutdownCancel()
-
-				grpcShutdown := make(chan struct{}, 1)
-				go func() {
-					grpcServer.GracefulStop()
-					grpcShutdown <- struct{}{}
-				}()
-
-				httpServer.Shutdown(shutdownCtx)
-				select {
-				case <-grpcShutdown:
-				case <-shutdownCtx.Done():
-					grpcServer.Stop()
-				}
-		}()
-	*/
 	//Run HTTP server
 	go func() {
 		defer a.appCancel()
